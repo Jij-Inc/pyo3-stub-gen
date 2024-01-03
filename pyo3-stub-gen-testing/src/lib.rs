a7a984d6ff0ee6af9140b40233637cb6606579c8
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-01-03 18:35:57 +0900

    WIP: Stub file generator executable

--- pyo3-stub-gen-testing/src/lib.rs
+++ pyo3-stub-gen-testing/src/lib.rs
@@ -1,5 +1,9 @@
 use pyo3::prelude::*;
 
+#[cfg(feature = "stub_gen")]
+use pyo3_stub_gen::derive::*;
+
+#[cfg_attr(feature = "stub_gen", gen_stub_pyfunction)]
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
