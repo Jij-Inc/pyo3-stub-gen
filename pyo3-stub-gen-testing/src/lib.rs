8d05a0489da3ce7ce2e01562bf97b9764b955e2d
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-01-05 17:18:23 +0900

    Remove feature flag for stub_gen

--- pyo3-stub-gen-testing/src/lib.rs
+++ pyo3-stub-gen-testing/src/lib.rs
@@ -1,34 +1,28 @@
 use pyo3::prelude::*;
+use pyo3_stub_gen::{derive::gen_stub_pyfunction, StubInfo};
+use std::{env, path::*};
 
-#[cfg(feature = "stub_gen")]
-use pyo3_stub_gen::derive::*;
+/// Gather information to generate stub files
+pub fn stub_info() -> pyo3_stub_gen::Result<StubInfo> {
+    let manifest_dir: &Path = env!("CARGO_MANIFEST_DIR").as_ref();
+    StubInfo::from_pyproject_toml(manifest_dir.join("pyproject.toml"))
+}
 
 /// Returns the sum of two numbers as a string.
-///
-/// Test of running doc-test
-///
-/// ```rust
-/// assert_eq!(2 + 2, 4);
-/// ```
-#[cfg_attr(feature = "stub_gen", gen_stub_pyfunction)]
+#[gen_stub_pyfunction]
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+/// Initializes the Python module
 #[pymodule]
 fn pyo3_stub_gen_testing(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
 
-#[cfg(feature = "stub_gen")]
-pub fn stub_info() -> pyo3_stub_gen::Result<pyo3_stub_gen::StubInfo> {
-    use std::{env, path::*};
-    let manifest_dir: &Path = env!("CARGO_MANIFEST_DIR").as_ref();
-    pyo3_stub_gen::StubInfo::from_pyproject_toml(manifest_dir.join("pyproject.toml"))
-}
-
+/// Test of unit test for testing link problem
 #[cfg(test)]
 mod test {
     #[test]
