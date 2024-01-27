ef7118b9cbb5436556b1df64a8f12b2e535de66c
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-01-27 17:27:07 +0900

    Fix tests

--- pyo3-stub-gen-testing-mixed/src/lib.rs
+++ pyo3-stub-gen-testing-mixed/src/lib.rs
@@ -17,7 +17,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// Initializes the Python module
 #[pymodule]
-fn pyo3_stub_gen_testing_mixed(_py: Python, m: &PyModule) -> PyResult<()> {
+fn my_rust_pkg(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
--- pyo3-stub-gen-testing-mixed/tests/test_python.py
+++
@@ -1,5 +0,0 @@
-import pyo3_stub_gen_testing_pure
-
-
-def test_sum_as_string():
-    assert pyo3_stub_gen_testing_pure.sum_as_string(1, 2) == "3"
