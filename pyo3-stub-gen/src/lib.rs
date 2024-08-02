325b092704b1bd2fe9441fa11b764e9e1f9765d6
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-08-02 13:58:46 +0900

    Remove feature gate

--- pyo3-stub-gen/src/lib.rs
+++ pyo3-stub-gen/src/lib.rs
@@ -1,8 +1,5 @@
-///
-#[cfg(feature = "derive")]
-pub use pyo3_stub_gen_derive as derive;
-
-pub use inventory; // re-export to use in generated code
+pub use inventory;
+pub use pyo3_stub_gen_derive as derive; // re-export to use in generated code
 
 mod generate;
 mod pyproject;
