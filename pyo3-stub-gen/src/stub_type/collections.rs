0f391a9557a8337afc257a92ca8e91435f6c1697
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-08-20 13:58:41 +0900

    Allow custom random state for HashMap

--- pyo3-stub-gen/src/stub_type/collections.rs
+++ pyo3-stub-gen/src/stub_type/collections.rs
@@ -56,46 +56,49 @@ impl<T: PyStubType> PyStubType for Vec<T> {
     }
 }
 
-macro_rules! impl_map {
-    ($map:ident) => {
-        impl<Key: PyStubType, Value: PyStubType> PyStubType for $map<Key, Value> {
-            fn type_input() -> TypeInfo {
-                let TypeInfo {
-                    name: key_name,
-                    mut import,
-                } = Key::type_input();
-                let TypeInfo {
-                    name: value_name,
-                    import: value_import,
-                } = Value::type_input();
-                import.extend(value_import);
-                import.insert("typing".into());
-                TypeInfo {
-                    name: format!("typing.Mapping[{}, {}]", key_name, value_name),
-                    import,
-                }
+macro_rules! impl_map_inner {
+    () => {
+        fn type_input() -> TypeInfo {
+            let TypeInfo {
+                name: key_name,
+                mut import,
+            } = Key::type_input();
+            let TypeInfo {
+                name: value_name,
+                import: value_import,
+            } = Value::type_input();
+            import.extend(value_import);
+            import.insert("typing".into());
+            TypeInfo {
+                name: format!("typing.Mapping[{}, {}]", key_name, value_name),
+                import,
             }
-            fn type_output() -> TypeInfo {
-                let TypeInfo {
-                    name: key_name,
-                    mut import,
-                } = Key::type_output();
-                let TypeInfo {
-                    name: value_name,
-                    import: value_import,
-                } = Value::type_output();
-                import.extend(value_import);
-                TypeInfo {
-                    name: format!("dict[{}, {}]", key_name, value_name),
-                    import,
-                }
+        }
+        fn type_output() -> TypeInfo {
+            let TypeInfo {
+                name: key_name,
+                mut import,
+            } = Key::type_output();
+            let TypeInfo {
+                name: value_name,
+                import: value_import,
+            } = Value::type_output();
+            import.extend(value_import);
+            TypeInfo {
+                name: format!("dict[{}, {}]", key_name, value_name),
+                import,
             }
         }
     };
 }
 
-impl_map!(HashMap);
-impl_map!(BTreeMap);
+impl<Key: PyStubType, Value: PyStubType> PyStubType for BTreeMap<Key, Value> {
+    impl_map_inner!();
+}
+
+impl<Key: PyStubType, Value: PyStubType, State> PyStubType for HashMap<Key, Value, State> {
+    impl_map_inner!();
+}
 
 macro_rules! impl_tuple {
     ($($T:ident),*) => {
