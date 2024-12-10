c93b11dfbda6126345cec692970cd85eb3e3d03b
Hiromi Ishii <konn.jinro@gmail.com>
2024-12-10 14:32:44 +0900

    fix: Track all submodules instead of one (#106)
    Currently , submodule relation is tracked as `map: BTreeMap<String,
    String>` in `register_submodules` function:
    
    
    https://github.com/Jij-Inc/pyo3-stub-gen/blob/bb791fb6d20e6c019aef32437d59a9b6349ff19e/pyo3-stub-gen/src/generate/stub_info.rs#L70-L85
    
    This resulted in wrong behaviour when a module has multiple submodules -
    it contains only one `from . import submod` for the last processed
    submodule.
    This PR fixes this by using `BTreeMap<String, BTreeSet<String>>`
    instead. It also adds `examples/mixed_sub_multiple` case to check the
    intended behaviour for this case.

---
+++ examples/mixed_sub_multiple/src/lib.rs
@@ -0,0 +1,57 @@
+use pyo3::prelude::*;
+use pyo3_stub_gen::{define_stub_info_gatherer, derive::*};
+
+#[gen_stub_pyfunction(module = "mixed_sub_multiple.main_mod.mod_a")]
+#[pyfunction(name = "greet_a")]
+pub fn greet_a() {
+    println!("Hello from mod_A!")
+}
+
+#[gen_stub_pyfunction(module = "mixed_sub_multiple.main_mod")]
+#[pyfunction(name = "greet_main")]
+pub fn greet_main() {
+    println!("Hello from main_mod!")
+}
+
+#[gen_stub_pyfunction(module = "mixed_sub_multiple.main_mod.mod_b")]
+#[pyfunction(name = "greet_b")]
+pub fn greet_b() {
+    println!("Hello from mod_B!")
+}
+
+#[pymodule]
+fn main_mod(m: &Bound<PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(greet_main, m)?)?;
+    mod_a(m)?;
+    mod_b(m)?;
+    Ok(())
+}
+
+#[pymodule]
+fn mod_a(parent: &Bound<PyModule>) -> PyResult<()> {
+    let py = parent.py();
+    let sub = PyModule::new_bound(py, "mod_a")?;
+    sub.add_function(wrap_pyfunction!(greet_a, &sub)?)?;
+    parent.add_submodule(&sub)?;
+    Ok(())
+}
+
+#[pymodule]
+fn mod_b(parent: &Bound<PyModule>) -> PyResult<()> {
+    let py = parent.py();
+    let sub = PyModule::new_bound(py, "mod_b")?;
+    sub.add_function(wrap_pyfunction!(greet_b, &sub)?)?;
+    parent.add_submodule(&sub)?;
+    Ok(())
+}
+
+define_stub_info_gatherer!(stub_info);
+
+/// Test of unit test for testing link problem
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        assert_eq!(2 + 2, 4);
+    }
+}
