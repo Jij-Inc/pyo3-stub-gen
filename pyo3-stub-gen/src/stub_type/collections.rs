25cb9f243be5cc702a174f9625c46193201cdf1c
junzhuo <junzhuo22@g.ucla.edu>
2025-01-15 20:44:28 -0800

    add `indexmap` feature

--- pyo3-stub-gen/src/stub_type/collections.rs
+++ pyo3-stub-gen/src/stub_type/collections.rs
@@ -103,6 +103,13 @@ impl<T: PyStubType> PyStubType for BTreeSet<T> {
     }
 }
 
+#[cfg(feature = "indexmap")]
+impl<T: PyStubType, State> PyStubType for indexmap::IndexSet<T, State> {
+    fn type_output() -> TypeInfo {
+        HashSet::<T, State>::type_output()
+    }
+}
+
 macro_rules! impl_map_inner {
     () => {
         fn type_input() -> TypeInfo {
@@ -144,6 +151,13 @@ impl<Key: PyStubType, Value: PyStubType> PyStubType for BTreeMap<Key, Value> {
 }
 
 impl<Key: PyStubType, Value: PyStubType, State> PyStubType for HashMap<Key, Value, State> {
+    impl_map_inner!();
+}
+
+#[cfg(feature = "indexmap")]
+impl<Key: PyStubType, Value: PyStubType, State> PyStubType
+    for indexmap::IndexMap<Key, Value, State>
+{
     impl_map_inner!();
 }
 
