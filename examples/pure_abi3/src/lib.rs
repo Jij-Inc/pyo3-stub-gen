e3648df2d27161f658f6fbd60f23af0a2d2408e5
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-05-31 17:46:57 +0900

    Support ABI3 case (#187)
    Resolve #184, integrated with #206
    
    - Detect `Py_LIMITED_API` flag from `pyo3-build-config` as done in #206 
    - Add `examples/pure_abi3` to test the above mechanism works.
    
    ---------
    
    Co-authored-by: junzhuo <junzhuo22@g.ucla.edu>

--- examples/pure_abi3/src/lib.rs
+++ examples/pure_abi3/src/lib.rs
@@ -34,10 +34,6 @@ fn create_dict(n: usize) -> HashMap<usize, Vec<usize>> {
 }
 
 #[gen_stub_pyclass]
-#[pyclass(extends=PyDate)]
-struct MyDate;
-
-#[gen_stub_pyclass]
 #[pyclass(subclass)]
 #[derive(Debug)]
 struct A {
@@ -75,32 +71,7 @@ fn create_a(x: usize) -> A {
 #[derive(Debug)]
 struct B;
 
-/// `C` only impl `FromPyObject`
-#[derive(Debug)]
-struct C {
-    x: usize,
-}
-#[gen_stub_pyfunction]
-#[pyfunction(signature = (c=None))]
-fn print_c(c: Option<C>) {
-    if let Some(c) = c {
-        println!("{}", c.x);
-    } else {
-        println!("None");
-    }
-}
-impl FromPyObject<'_> for C {
-    fn extract_bound(ob: &Bound<'_, PyAny>) -> PyResult<Self> {
-        Ok(C { x: ob.extract()? })
-    }
-}
-impl pyo3_stub_gen::PyStubType for C {
-    fn type_output() -> pyo3_stub_gen::TypeInfo {
-        usize::type_output()
-    }
-}
-
-create_exception!(pure, MyError, PyRuntimeError);
+create_exception!(pure_abi3, MyError, PyRuntimeError);
 
 /// Returns the length of the string.
 #[gen_stub_pyfunction]
@@ -140,7 +111,6 @@ pub enum Number {
 #[pyo3(rename_all = "UPPERCASE")]
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum NumberRenameAll {
-    /// Float variant
     Float,
     Integer,
 }
@@ -161,7 +131,7 @@ impl Number {
     }
 }
 
-module_variable!("pure", "MY_CONSTANT", usize);
+module_variable!("pure_abi3", "MY_CONSTANT", usize);
 
 // Test if non-any PyObject Target can be a default value
 #[gen_stub_pyfunction]
@@ -173,19 +143,17 @@ fn default_value(num: Number) -> Number {
 
 /// Initializes the Python module
 #[pymodule]
-fn pure(m: &Bound<PyModule>) -> PyResult<()> {
+fn pure_abi3(m: &Bound<PyModule>) -> PyResult<()> {
     m.add("MyError", m.py().get_type::<MyError>())?;
     m.add("MY_CONSTANT", 19937)?;
     m.add_class::<A>()?;
     m.add_class::<B>()?;
-    m.add_class::<MyDate>()?;
     m.add_class::<Number>()?;
     m.add_class::<NumberRenameAll>()?;
     m.add_function(wrap_pyfunction!(sum, m)?)?;
     m.add_function(wrap_pyfunction!(create_dict, m)?)?;
     m.add_function(wrap_pyfunction!(read_dict, m)?)?;
     m.add_function(wrap_pyfunction!(create_a, m)?)?;
-    m.add_function(wrap_pyfunction!(print_c, m)?)?;
     m.add_function(wrap_pyfunction!(str_len, m)?)?;
     m.add_function(wrap_pyfunction!(echo_path, m)?)?;
     m.add_function(wrap_pyfunction!(ahash_dict, m)?)?;
