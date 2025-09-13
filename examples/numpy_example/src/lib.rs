1999d51a4c18317e9c6a7e3167f4472997f364ee
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-09-14 02:07:24 +0900

    Format

--- examples/numpy_example/src/lib.rs
+++ examples/numpy_example/src/lib.rs
@@ -2,16 +2,21 @@ use numpy::{AllowTypeChange, PyArray1, PyArrayLike1, TypeMustMatch};
 use pyo3::prelude::*;
 use pyo3_stub_gen::{define_stub_info_gatherer, derive::*};
 
-
 #[gen_stub_pyfunction]
 #[pyfunction]
-fn np_allow_type_change<'py>(py: Python<'py>, x: PyArrayLike1<'py, f64, AllowTypeChange>) -> Bound<'py, PyArray1<f64>> {
+fn np_allow_type_change<'py>(
+    py: Python<'py>,
+    x: PyArrayLike1<'py, f64, AllowTypeChange>,
+) -> Bound<'py, PyArray1<f64>> {
     PyArray1::<f64>::from_array(py, &x.as_array())
 }
 
 #[gen_stub_pyfunction]
 #[pyfunction]
-fn np_type_must_match<'py>(py: Python<'py>, x: PyArrayLike1<'py, i16, TypeMustMatch>) -> Bound<'py, PyArray1<i16>> {
+fn np_type_must_match<'py>(
+    py: Python<'py>,
+    x: PyArrayLike1<'py, i16, TypeMustMatch>,
+) -> Bound<'py, PyArray1<i16>> {
     PyArray1::<i16>::from_array(py, &x.as_array())
 }
 
