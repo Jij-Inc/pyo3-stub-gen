0255fa07ccba2d0759bf7f0a859498b2d54ea0d4
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-09-27 16:25:26 +0900

    clippy fix

--- examples/feature_gated/src/gen_stub_default.rs
+++ examples/feature_gated/src/gen_stub_default.rs
@@ -2,17 +2,12 @@ use pyo3::prelude::*;
 
 #[cfg_attr(feature = "stub-gen", pyo3_stub_gen::derive::gen_stub_pyclass)]
 #[pyclass]
+#[derive(Default)]
 pub struct A {
     #[pyo3(get, set)]
     #[cfg_attr(feature = "stub-gen", gen_stub(default = A::default().x))]
     x: usize,
     y: usize,
-}
-
-impl Default for A {
-    fn default() -> Self {
-        A { x: 0, y: 0 }
-    }
 }
 
 #[cfg_attr(feature = "stub-gen", pyo3_stub_gen::derive::gen_stub_pymethods)]
