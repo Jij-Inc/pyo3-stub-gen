038803538d57823d0e3974d099927769b7662ae4
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-10-11 16:46:15 +0900

    Split overriding example into a separate file

--- examples/pure/src/lib.rs
+++ examples/pure/src/lib.rs
@@ -1,7 +1,10 @@
 #![allow(deprecated)]
 
 mod custom_exceptions;
+mod overriding;
+
 use custom_exceptions::*;
+use overriding::*;
 
 #[cfg_attr(target_os = "macos", doc = include_str!("../../../README.md"))]
 mod readme {}
@@ -334,48 +337,6 @@ fn deprecated_function() {
 #[pyo3(signature = (num = Number::Float))]
 fn default_value(num: Number) -> Number {
     num
-}
-
-#[gen_stub_pyfunction]
-#[pyfunction]
-#[gen_stub(override_return_type(type_repr="collections.abc.Callable[[str], typing.Any]", imports=("collections.abc", "typing")))]
-fn fn_override_type<'a>(
-    #[gen_stub(override_type(type_repr="collections.abc.Callable[[str], typing.Any]", imports=("collections.abc", "typing")))]
-    cb: Bound<'a, PyAny>,
-) -> PyResult<Bound<'a, PyAny>> {
-    cb.call1(("Hello!",))?;
-    Ok(cb)
-}
-#[gen_stub_pyclass]
-#[pyclass]
-struct OverrideType {
-    num: isize,
-}
-
-#[gen_stub_pymethods]
-#[pymethods]
-impl OverrideType {
-    #[gen_stub(override_return_type(type_repr="typing_extensions.Never", imports=("typing_extensions")))]
-    fn error(&self) -> PyResult<()> {
-        Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
-            "I'm an error!",
-        ))
-    }
-
-    #[getter]
-    #[gen_stub(override_return_type(type_repr = "int"))]
-    fn get_num(&self) -> PyResult<Py<PyAny>> {
-        Python::attach(|py| self.num.into_py_any(py))
-    }
-
-    #[setter]
-    fn set_num(
-        &mut self,
-        #[gen_stub(override_type(type_repr = "str"))] value: Py<PyAny>,
-    ) -> PyResult<()> {
-        self.num = Python::attach(|py| value.extract::<String>(py))?.parse::<isize>()?;
-        Ok(())
-    }
 }
 
 // Test for `@overload` decorator generation
---
+++ examples/pure/src/overriding.rs
@@ -0,0 +1,44 @@
+use pyo3::{prelude::*, IntoPyObjectExt};
+use pyo3_stub_gen::derive::*;
+
+#[gen_stub_pyfunction]
+#[pyfunction]
+#[gen_stub(override_return_type(type_repr="collections.abc.Callable[[str], typing.Any]", imports=("collections.abc", "typing")))]
+pub fn fn_override_type<'a>(
+    #[gen_stub(override_type(type_repr="collections.abc.Callable[[str], typing.Any]", imports=("collections.abc", "typing")))]
+    cb: Bound<'a, PyAny>,
+) -> PyResult<Bound<'a, PyAny>> {
+    cb.call1(("Hello!",))?;
+    Ok(cb)
+}
+#[gen_stub_pyclass]
+#[pyclass]
+pub struct OverrideType {
+    num: isize,
+}
+
+#[gen_stub_pymethods]
+#[pymethods]
+impl OverrideType {
+    #[gen_stub(override_return_type(type_repr="typing_extensions.Never", imports=("typing_extensions")))]
+    fn error(&self) -> PyResult<()> {
+        Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
+            "I'm an error!",
+        ))
+    }
+
+    #[getter]
+    #[gen_stub(override_return_type(type_repr = "int"))]
+    fn get_num(&self) -> PyResult<Py<PyAny>> {
+        Python::attach(|py| self.num.into_py_any(py))
+    }
+
+    #[setter]
+    fn set_num(
+        &mut self,
+        #[gen_stub(override_type(type_repr = "str"))] value: Py<PyAny>,
+    ) -> PyResult<()> {
+        self.num = Python::attach(|py| value.extract::<String>(py))?.parse::<isize>()?;
+        Ok(())
+    }
+}
